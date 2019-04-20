90b53c9b93c9a89b7966b11b047abd5e2d6923fd
Acris Liu <acrisliu@gmail.com>
2019-04-20 23:29:17 +0800

    Minor update for post upgrade script

--- tools/post_upgrade.sh
+++ tools/post_upgrade.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 SS_MERLIN_HOME=/opt/share/ss-merlin
+DNSMASQ_CONFIG_DIR=${SS_MERLIN_HOME}/etc/dnsmasq.d
 
 # Anything should do after upgrade
 echo "2019/04/20 Update GFW list after upgrade"
-${SS_MERLIN_HOME}/scripts/update_gfwlist.sh
+if [[ ! -f ${DNSMASQ_CONFIG_DIR}/dnsmasq_gfwlist_ipset.conf.bak ]]; then
+  ${SS_MERLIN_HOME}/scripts/update_gfwlist.sh
+fi
 
 echo "2019/04/20 Destroy old ipset for name changing"
-if [[ -f /tmp/ss-merlin-is-run ]]; then
+if ipset list CHINAIP &> /dev/null; then
   ipset destroy CHINAIP 2> /dev/null
+fi
+if ipset list CHINAIPS &> /dev/null; then
   ipset destroy CHINAIPS 2> /dev/null
 fi
