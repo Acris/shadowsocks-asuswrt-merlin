ba9011d97c1dea2cf71c216581c3ce20b741f7b0
Acris Liu <acrisliu@gmail.com>
2019-04-20 19:26:46 +0800

    Destroy old ipset for name changing

--- tools/post_upgrade.sh
+++ tools/post_upgrade.sh
@@ -3,6 +3,9 @@
 SS_MERLIN_HOME=/opt/share/ss-merlin
 
 # Anything should do after upgrade
-
-# 2019/04/20 Update GFW list after upgrade
+echo "2019/04/20 Update GFW list after upgrade"
 ${SS_MERLIN_HOME}/scripts/update_gfwlist.sh
+
+echo "2019/04/20 Destroy old ipset for name changing"
+ipset destroy CHINAIP 2> /dev/null
+ipset destroy CHINAIPS 2> /dev/null
