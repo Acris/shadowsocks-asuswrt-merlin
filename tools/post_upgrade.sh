baa52573064e8d33e71667e9fea779a3c58fe460
Acris Liu <acrisliu@gmail.com>
2019-04-20 21:40:39 +0800

    Minor update

--- tools/post_upgrade.sh
+++ tools/post_upgrade.sh
@@ -7,5 +7,7 @@ echo "2019/04/20 Update GFW list after upgrade"
 ${SS_MERLIN_HOME}/scripts/update_gfwlist.sh
 
 echo "2019/04/20 Destroy old ipset for name changing"
-ipset destroy CHINAIP 2> /dev/null
-ipset destroy CHINAIPS 2> /dev/null
+if [[ -f /tmp/ss-merlin-is-run ]]; then
+  ipset destroy CHINAIP 2> /dev/null
+  ipset destroy CHINAIPS 2> /dev/null
+fi
