cb46aade5ca32f99dd4ee58ed7d02d5d7232cec3
Acris Liu <acrisliu@gmail.com>
2019-04-20 19:09:04 +0800

    Minor fix for update GFW list script

--- scripts/update_gfwlist.sh
+++ scripts/update_gfwlist.sh
@@ -3,7 +3,7 @@
 update_gfwlist() {
   SS_MERLIN_HOME=/opt/share/ss-merlin
 
-  wget -O- 'https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist_ipset.conf' | sed "/127.0.0.1#5353/d" > ${SS_MERLIN_HOME}/rules/dnsmasq_gfwlist_ipset.conf.bak
+  wget -O - 'https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist_ipset.conf' | sed "/127.0.0.1#5353/d" > ${SS_MERLIN_HOME}/etc/dnsmasq.d/dnsmasq_gfwlist_ipset.conf.bak
 
   echo 'Update GFW list done.'
 }
