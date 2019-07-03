0d5d4e44aa7f3efed5babe0ff8a38bb81aa10914
Acris Liu <acrisliu@gmail.com>
2019-07-03 21:21:14 +0800

    Update statistics domain

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -33,7 +33,7 @@ upgrade() {
 
     # Statistics
     cd ${SS_MERLIN_HOME}
-    wget --quiet --method POST --output-document - "https://ss-merlin-stats.iloli.li/stats?hash=`git rev-parse --short HEAD`"
+    wget --quiet --method POST --output-document - "https://ss-merlin.iloli.li/stats?hash=`git rev-parse --short HEAD`"
 
     echo -e "$ansi_green"
     echo "   ______           __                        __       ";
