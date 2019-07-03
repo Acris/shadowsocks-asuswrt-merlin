2c26271ae90b06693ff08b6bb3f2042d55a6522c
Acris Liu <acrisliu@gmail.com>
2019-07-04 01:29:04 +0800

    Minor update

--- tools/pre_upgrade.sh
+++ tools/pre_upgrade.sh
@@ -7,9 +7,9 @@ SS_MERLIN_HOME=/opt/share/ss-merlin
 echo -e "Back up shadowsocks configuration file"
 cd ${SS_MERLIN_HOME}/etc/shadowsocks
 cp config.json config.json.bak
-git checkout config.json
+rm config.json
 
 echo -e "Back up ss-merlin configuration file"
 cd ${SS_MERLIN_HOME}/etc
 cp ss-merlin.conf ss-merlin.conf.bak
-git checkout ss-merlin.conf
+rm ss-merlin.conf
