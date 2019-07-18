813c4e419c527ccea8f0635d12298143ef4e556d
Acris Liu <acrisliu@gmail.com>
2019-07-18 12:25:50 +0800

    Remove mac address for user's privacy

--- tools/statistics.sh
+++ tools/statistics.sh
@@ -3,5 +3,4 @@
 cd /opt/share/ss-merlin
 branch=$(git rev-parse --abbrev-ref HEAD)
 hash=$(git rev-parse --short HEAD)
-mac=$(cat /sys/class/net/eth0/address)
-wget --quiet --method POST --output-document - "https://ss-merlin.iloli.li/stats?version=${branch}_${hash}&mac=${mac}"
+wget --quiet --method POST --output-document - "https://ss-merlin.iloli.li/stats?version=${branch}_${hash}"
