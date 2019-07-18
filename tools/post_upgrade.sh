33999c56f6391c5429da45a48883f8ad009cbf0f
Acris Liu <acrisliu@gmail.com>
2019-07-18 09:40:41 +0800

    Clean up

--- tools/post_upgrade.sh
+++ tools/post_upgrade.sh
@@ -3,10 +3,3 @@
 SS_MERLIN_HOME=/opt/share/ss-merlin
 
 # Anything should do after upgrade
-DNSMASQ_CONFIG_DIR=${SS_MERLIN_HOME}/etc/dnsmasq.d
-if [[ -f ${DNSMASQ_CONFIG_DIR}/whitelist-domains.china.conf ]]; then
-  rm -f ${DNSMASQ_CONFIG_DIR}/whitelist-domains.china.conf
-fi
-if [[ -f ${DNSMASQ_CONFIG_DIR}/blocklist-domains.china.conf ]]; then
-  rm -f ${DNSMASQ_CONFIG_DIR}/blocklist-domains.china.conf
-fi
