f2438421a9a2794b2e82e03044fec925b63bafb5
Acris Liu <acrisliu@gmail.com>
2019-08-06 22:44:39 +0800

    Use China IPs from ipip.net

--- scripts/update_gfwlist.sh
+++ scripts/update_gfwlist.sh
@@ -4,7 +4,7 @@ update_gfwlist() {
   SS_MERLIN_HOME=/opt/share/ss-merlin
   DNSMASQ_CONFIG_DIR=${SS_MERLIN_HOME}/etc/dnsmasq.d
 
-  wget -O - 'https://ss-merlin.iloli.li/proxy-github-cokebar/gfwlist2dnsmasq/dnsmasq_gfwlist_ipset.conf' | sed "/127.0.0.1#5353/d" >${DNSMASQ_CONFIG_DIR}/dnsmasq_gfwlist_ipset.conf.bak
+  wget -O - 'https://ss-merlin.iloli.li/proxy-github-raw/cokebar/gfwlist2dnsmasq/gh-pages/dnsmasq_gfwlist_ipset.conf' | sed "/127.0.0.1#5353/d" > ${DNSMASQ_CONFIG_DIR}/dnsmasq_gfwlist_ipset.conf.bak
 
   echo 'Update GFW list done.'
 }
