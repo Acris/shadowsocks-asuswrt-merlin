031fb7ca2003288f55c6471fdeaa7e16e1003c07
Acris Liu <acrisliu@gmail.com>
2019-10-16 09:47:35 +0800

    Refine service alive checking

--- scripts/start_all_services.sh
+++ scripts/start_all_services.sh
@@ -3,17 +3,23 @@
 SS_MERLIN_HOME=/opt/share/ss-merlin
 
 # Start if process not running
-unbound_pid=$(pidof unbound)
-if [[ -z "$unbound_pid" ]]; then
-  unbound -c ${SS_MERLIN_HOME}/etc/unbound/unbound.conf
-fi
-
 ss_pid=$(pidof ss-redir)
 if [[ -z "$ss_pid" ]]; then
   if [[ ! -f ${SS_MERLIN_HOME}/etc/shadowsocks/config.json ]]; then
     cp ${SS_MERLIN_HOME}/etc/shadowsocks/config.sample.json ${SS_MERLIN_HOME}/etc/shadowsocks/config.json
   fi
   ss-redir -c ${SS_MERLIN_HOME}/etc/shadowsocks/config.json -f /opt/var/run/ss-redir.pid
+fi
+
+v2ray_pid=$(pidof v2ray-plugin)
+if [[ -z "$v2ray_pid" ]]; then
+  killall ss-redir 2>/dev/null
+  ss-redir -c ${SS_MERLIN_HOME}/etc/shadowsocks/config.json -f /opt/var/run/ss-redir.pid
+fi
+
+unbound_pid=$(pidof unbound)
+if [[ -z "$unbound_pid" ]]; then
+  unbound -c ${SS_MERLIN_HOME}/etc/unbound/unbound.conf
 fi
 
 echo "All service started."
