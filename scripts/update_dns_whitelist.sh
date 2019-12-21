2e402833245f060ddd655a1e1c2574c8da8ab84d
Acris Liu <acrisliu@gmail.com>
2019-12-21 17:47:24 +0800

    Use jsdelivr CDN for better speed

--- scripts/update_dns_whitelist.sh
+++ scripts/update_dns_whitelist.sh
@@ -7,9 +7,9 @@ update_dns_whitelist() {
   GOOGLE_CONFIG=${SS_MERLIN_HOME}/etc/dnsmasq.d/google.china.conf
   APPLE_CONFIG=${SS_MERLIN_HOME}/etc/dnsmasq.d/apple.china.conf
 
-  wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf -O ${ACCELERATED_CONFIG}.bak
-  wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf -O ${GOOGLE_CONFIG}.bak
-  wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf -O ${APPLE_CONFIG}.bak
+  wget https://cdn.jsdelivr.net/gh/felixonmars/dnsmasq-china-list@master/accelerated-domains.china.conf -O ${ACCELERATED_CONFIG}.bak
+  wget https://cdn.jsdelivr.net/gh/felixonmars/dnsmasq-china-list@master/google.china.conf -O ${GOOGLE_CONFIG}.bak
+  wget https://cdn.jsdelivr.net/gh/felixonmars/dnsmasq-china-list@master/apple.china.conf -O ${APPLE_CONFIG}.bak
 
   echo 'Update DNS whitelist done.'
 }
--- scripts/update_ip_whitelist.sh
+++ scripts/update_ip_whitelist.sh
@@ -3,7 +3,7 @@
 update_ip_whitelist() {
   SS_MERLIN_HOME=/opt/share/ss-merlin
 
-  wget -O - 'https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt' | sed '/^#/d' > ${SS_MERLIN_HOME}/rules/chinadns_chnroute.txt.bak
+  wget -O - 'https://cdn.jsdelivr.net/gh/17mon/china_ip_list@master/china_ip_list.txt' | sed '/^#/d' > ${SS_MERLIN_HOME}/rules/chinadns_chnroute.txt.bak
 
   echo 'Update IP whitelist done.'
 }
