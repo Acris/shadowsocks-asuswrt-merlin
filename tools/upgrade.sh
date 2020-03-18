74eb509a5e2c6a91bbcceecebad649f45b6a3f0c
Acris Liu <acrisliu@gmail.com>
2020-03-18 15:35:10 +0800

    Update script URL

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -10,7 +10,7 @@ upgrade() {
 
   rm -f ${SS_MERLIN_HOME}/tools/do_upgrade.sh
 
-  wget https://raw.githubusercontent.com/Acris/shadowsocks-asuswrt-merlin/master/tools/do_upgrade.sh -O ${SS_MERLIN_HOME}/tools/do_upgrade.sh
+  wget https://raw.github.com/Acris/shadowsocks-asuswrt-merlin/master/tools/do_upgrade.sh -O ${SS_MERLIN_HOME}/tools/do_upgrade.sh
 
   chmod +x ${SS_MERLIN_HOME}/tools/do_upgrade.sh
 
